ee4c0cd1a517771b4a1de638f1b869dc52065352
MatanAvitan-98 <matan3751@gmail.com>
2021-03-18 22:13:28 +0200

    get only data from 20 minutes ago

--- db_api/app/resources/db_api.py
+++ db_api/app/resources/db_api.py
@@ -2,7 +2,7 @@ from flask_restful import Resource
 from flask import request
 from json import loads
 from db_api.app.models.scan_model import db, ScanModel
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class DBApi(Resource):
@@ -16,5 +16,11 @@ class DBApi(Resource):
         return {}
 
     def get(self, scan_id):
-        scan_status = ScanModel.query.filter_by(scan_id=scan_id).first()
-        return {"status": scan_status.status, "scan_id": scan_id}
+        last_twenty_minutes = datetime.now() - timedelta(minutes=20)
+        scan_status = ScanModel.query.filter(ScanModel.insertion_time > last_twenty_minutes).filter_by(
+            scan_id=scan_id).first()
+        if scan_status is None:
+            status = "NOT FOUND"
+        else:
+            status = scan_status.status
+        return {"status": status, "scan_id": scan_id}
