b5de6926893b02378bb3867e3ac8c8870c12b66b
MatanAvitan-98 <matan3751@gmail.com>
2021-03-19 13:31:53 +0200

    add http status

--- db_api/app/resources/resource_utils.py
+++ db_api/app/resources/resource_utils.py
@@ -1,8 +1,12 @@
+from flask_restful import abort
+from http import HTTPStatus
+
+
 def error_handler(func):
     def handler(*args, **kwargs):
         try:
             return func(*args, **kwargs)
         except Exception as e:
-            return {"result": "Failed", "error": str(e)}
+            abort(HTTPStatus.INTERNAL_SERVER_ERROR, message=str(e))
 
     return handler
