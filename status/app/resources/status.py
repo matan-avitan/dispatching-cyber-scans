0835ebb8cd1b37944cd48fbb6ee5eaa83a3b6f58
MatanAvitan-98 <matan3751@gmail.com>
2021-03-19 15:10:36 +0200

    update status component with conf

--- status/app/resources/status.py
+++ status/app/resources/status.py
@@ -1,11 +1,14 @@
 import requests
+from status.app.conf import Conf
 from flask_restful import Resource
-from json import loads
-from flask import request
 
 
 class Status(Resource):
+    """
+    Status component is client api to know what is the status of a scan.
+    get - send a scan id and get the status of the scan.
+    """
 
     def get(self, scan_id):
-        scan_status = requests.get(f'http://127.0.0.1:8080/db-api/scan/{scan_id}/').json()
+        scan_status = requests.get(f'{Conf.BASE_API_URL}{Conf.GET_STATUS_URL}{scan_id}/').json()
         return {"result": scan_status}
