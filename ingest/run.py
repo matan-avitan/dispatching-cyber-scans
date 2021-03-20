feb2542ce16ac73f576f78cf7f49f6a2771a56f3
MatanAvitan-98 <matan3751@gmail.com>
2021-03-20 19:50:14 +0200

    add conf for multiprocessing

--- ingest/run.py
+++ ingest/run.py
@@ -1,6 +1,6 @@
 from ingest.app import create_app
-
+from ingest.app.conf import Conf
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5000, debug=False, threaded=True, processes=Conf.NUMBER_OF_REQUEST_PARALLEL)
